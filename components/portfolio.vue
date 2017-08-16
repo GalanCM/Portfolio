c52c80e6b8d9612af86d3bc92c12676e4e3b4ffd
Galan Montgomery <galan.c.m@gmail.com>
2017-08-16 12:12:30 -0400

    Complete desktop portfolio.

--- components/portfolio.vue
+++ components/portfolio.vue
@@ -1,9 +1,11 @@
 <template>
   <section>
     <h1>Portfolio</h1>
+
     <svg height="40" width="100%">
       <line x1="0" y1="28px" x2="100%" y2="28px" stroke-width="2px" stroke="rgb(12, 27, 42)"></line>
     </svg>
+
     <span class="nav-bg">
       <div class="thumb" :style="thumb_style"></div>
 
@@ -14,12 +16,25 @@
         Games
       </span>
     </span>
+
+    <div class="content" :style="{ 'transform': content_transform }">
+      <div class="panel">
+        <div v-for=" (site, key) in sites ">
+          <item :info=" site " :side=" key % 2 === 0 ? 'left' : 'right' "></item>
+        </div>
+      </div>
+      <div class="panel">
+        <h2>Timebomb</h2>
+      </div>
+    </div>
   </section>
 </template>
 
 <style lang="less" scoped>
   section {
     margin-top: 60px;
+    width: 100vw;
+    overflow-x: hidden;
   }
 
   h1 {
@@ -54,7 +69,7 @@
     font-weight: 600;
 
     &:not( .active ) {
-      color: saturate( lighten( rgb(12, 27, 42), 30% ), 10% );
+      color: saturate( lighten( rgb(12, 27, 42), 30% ), 50% );
       cursor: pointer;
     }
 
@@ -79,17 +94,95 @@
     top: 0;
     left: 0;
   }
+
+  .content {
+    width: 200vw;
+    top: -80px;
+    position: relative;
+    transition: 0.5s transform ease-in-out;
+  }
+
+  .panel {
+    width: 100vw;
+    padding: 20px 0;
+    float: left;
+
+    > div {
+      max-height: 100%;
+    }
+  }
 </style>
 
 <script>
+  import Item from "./portfolio_item.vue";
+
+  /* flow */
   export default {
+    components: { Item },
+
     data() {
       return {
         current_tab: "tab-websites",
         thumb_style: {
           left: 0,
           width: '50%'
-        }
+        },
+        content_transform: "translateX(0)",
+
+        sites: [
+          {
+            color: "#0067cf",
+            url: null,
+            image: null,
+            title: "This Site",
+            main: "This site is a work-in-progress showcase for my web development skills. " +
+                  "As of right now it hosts my portfolio, but I also plan to add a blog, with a login system and comments.",
+            tech: "I'm using this site to learn several new things. The foremost is gradual typing, using <a href='https://www.flow.org'>Flow</a> on the front-end and <a href='http://mypy-lang.org/'>MyPy</a> on the back. " +
+                  "When implementing the server, I also plan on strengthening my NoSQL skills with MongoDB.",
+            frontend: "Vue, Webpack, Babel, ESlint",
+            backend: "Python, Flask, Caddy"
+          },
+          {
+            color: "#994d00",
+            url: "https://www.isoteric-games.com",
+            image: "../images/isoteric-games.png",
+            title: "Isoteric Games",
+            main: "A responsive website that serves as a promotional web presence for my hobby games. Uses animated transitions and looks good on a mobile and desktop.",
+            tech: "The main goal for this site is to be informative, so the tech is fairly simple. The front-end is designed in Vue, with a thin back-end written in Go. Since the only use for a database would have been for the occasional blog post, I opted instead for writing them as meta-tagged Markdown files.",
+            frontend: "Webpack, Babel",
+            backend: "Caddy"
+          },
+          {
+            color: "#fd9001",
+            url: "https://www.childfund.org/thereturn/",
+            image: "../images/there-turn.png",
+            title: "Childfund VR | The Return",
+            main: "Informational website for Childfund’s VR experience: The Return. This is a single-page app that hosts the video and informs Childfund’s donors about its mission. Based on a design from the team artist, which I adapted for mobile.",
+            tech: "Another informational site, this time build on a CMS. This was my first experience with React, and contained a number of CSS transition effects. One of the biggest challenges, though, was cleanly integrating a Google VR video.",
+            frontend: "React, Webpack, Babel",
+            backend: null
+          },
+          {
+            color: "rgb(115, 168, 57)",
+            url: null,
+            image: "../images/getloaded-mobile.png",
+            title: "Getloaded Mobile 2.0",
+            main: "Getloaded Mobile was an iOS and Android apps used by over 20,000 people. I led the development of this app, and served as core architect and top-level UI designer.",
+            tech: "This began as a research project to replace our aging mobile apps. My team settled on a combination of Phonegap and Ractive, which allowed us quickly convert the templates from our mobile website into components for a single-page app.",
+            frontend: "Coffeescript, Less",
+            backend: null
+          }
+          // {
+          //   color: "rgb(253, 185, 19)",
+          //   url: null,
+          //   image: "../images/getloaded.png",
+          //   title: "Getloaded",
+          //   main: "Getloaded was an eCommerce site, used by over 100,000 independent truckers to find loads. Full-stack developer on a small team, where I played a major role in defining the interface for it’s mobile and admin interfaces, and built new back-end email testing and error handling systems.",
+          //   tech: "This was the first major project th",
+          //   frontend: "Coffeescript, jQuery, Backbone",
+          //   backend: "PHP, PHP Activerecord, Postgres"
+          // }
+        ]
       };
     },
 
@@ -106,6 +199,13 @@
           left: this.$refs[ tab ].offsetLeft + "px",
           width: this.$refs[ tab ].offsetWidth + "px"
         };
+
+        if ( tab === 'tab-games' ) {
+          this.content_transform = "translateX(-100vw)";
+        }
+        else {
+          this.content_transform = "translateX(0)";
+        }
       }
     }
   };
---
+++ components/portfolio_item.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="wrapper">
+    <div v-if=" info.image !== null && side === 'left' " :style=" 'box-shadow: 2px 1px 4px rgba(0,0,0,0.2), 10px 7px ' + info.color + ', 0 7px ' + info.color ">
+      <img :src=" info.image ">
+    </div>
+
+    <div class="details" :style="{ 'margin-left': margin.left + 'px', 'margin-right': margin.right + 'px' }">
+
+      <h2>
+        <span v-if=" info.url !== null ">
+          <a :href="info.url">{{ info.title }}</a>
+        </span>
+        <span v-else>
+          {{ info.title }}
+        </span>
+      </h2>
+
+      <p>
+        {{ info.main }}
+      </p>
+
+      <p class="tech">
+        <strong>&mdash; The Tech &mdash;</strong>
+      </p>
+
+      <p v-html=" info.tech "></p>
+
+      <ul v-if=" info.frontend !== null || info.backend !== null">
+        <em>As for the rest of the stack…</em>
+        <li><strong>Front-end:</strong> {{ info.frontend }}</li>
+        <li v-if=" info.backend !== null "><strong>Back-end:</strong> {{ info.backend }}</li>
+      </ul>
+    </div>
+
+    <div v-if=" info.image !== null && side === 'right' " :style=" 'box-shadow: 2px 1px 4px rgba(0,0,0,0.2), 10px 7px ' + info.color ">
+      <img :src=" info.image ">
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+  .wrapper {
+    width: 100%;
+    max-height: 350px;
+    display: flex;
+    margin-bottom: 100px;
+  }
+  img {
+    height: 100%;
+  }
+  .details {
+    display: flex;
+    flex-direction: column;
+    width: inherit;
+  }
+  h2 {
+    display: inline-block;
+    margin: 30px auto;
+    font-size: 35px;
+    font-weight: 300;
+    font-style: italic;
+
+    a {
+      text-decoration: none;
+    }
+  }
+  p {
+    padding: 0 30px;
+    margin: 5px 0;
+  }
+  .tech {
+    text-align: center;
+    margin: 15px 0 0px;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0 30px;
+  }
+  li {
+    padding-top: 5px;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        margin: {
+          left:  ( this.info.url === null || this.side === 'right' ) ? ( window.innerWidth - 1100 ) / 2 : 30,
+          right: ( this.info.url === null || this.side === 'left' ) ? ( window.innerWidth - 1100 ) / 2 : 30
+        }
+      };
+    },
+    props: [ 'info', 'side' ]
+  };
+</script>
