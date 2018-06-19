fe0d69855a9aed576c3737a4d3e881bee88e757f
Galan Montgomery <galan.c.m@gmail.com>
2018-06-19 13:46:52 -0400

    Implement new intro — static only.

--- src/components/nav_bar.vue
+++ src/components/nav_bar.vue
@@ -1,174 +1,94 @@
 <template>
-  <nav :style="{ transform:  uncollapsed ? '' : 'translateY( -100vh ) translateY('+ 50 +'px) ',  transition: intro_transitioning ? '1s transform 1s' : '', top: position+'px', 'position': position <= 0 ? 'fixed' : 'absolute' }">
-    <h1 class="name" :style=" {
-      transition: intro_transitioning ? '1s left linear, 1s bottom linear' : '',
-      bottom: uncollapsed ? 'calc(50vh + 5px)' : (-9)+'px',
-      left: uncollapsed ? '5vw' : '0',
-      'will-change': uncollapsed ? 'left, top' : '',
-    } ">
-      <object :data=" require( '../assets/header.svg') " type="image/svg+xml" :style="{ transition: intro_transitioning ? '1s transform ease-out' : '', transform: 'scale(' + scale_factor + ')', position: 'absolute', left: 0, bottom: 0, 'z-index': 1 }"></object>
-      <object :data=" require( '../assets/header.svg') " type="image/svg+xml" :style="{ transition: intro_transitioning ? '1s transform ease-out' : '', transform: 'scale(' + scale_factor + ')', position: 'absolute', left: '1px', bottom: '-1px', filter: 'brightness(0)' }"></object>
-    </h1>
-
-    <titles :show="titles_visible" @close_intro=" close_intro " @intro_complete="show_close"></titles>
-
-    <transition name="arrow" appear>
-      <continue :style=" 'transform: scale('+scale_factor*1.5+'); transform-origin: bottom left;'" v-if="close_visible"></continue>
-    </transition>
-
-    <transition name="fade-in" appear>
-      <NavLinks v-show="show_links"></NavLinks>
-    </transition>
-
-    
-    <transition v-on:enter="underline_enter" appear>
-      <svg class="underline" :height="12" :width="(underline_length)+'px'" :style=" 'position: absolute; bottom: '+(-10)+'px; left: 0; z-index: 0;'" v-show="!titles_visible">
-        <polygon :points="'0,0, '+ (underline_length) +',0, '+(underline_length-10)+','+(14)+', 0,'+(14)+''" fill="#820a0a"/>
-      </svg>
-    </transition>
-  </nav>
+  <div>
+    <nav>
+    </nav>
+    <div class="intro">
+      <div class="top">
+        <img class="name" src="../assets/header.svg" />
+      </div>
+      <div class="line"></div>
+      <div class="bottom">
+        <div class="titles">
+          <div class="web">Web UX Engineer</div>
+          <div class="and">&</div>
+          <div class="game">Game Developer</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
-  nav {
-    background-color: #001f3d;
-    color: white;
+.intro {
+  height: 100vh;
+  width: 100vw;
+
+  .top {
+    position: fixed;
+    width: 100vw;
+    height: 50vh;
+    min-height: fit-content;
     display: flex;
     flex-direction: column;
-    width: 100vw;
-    top: 0;
+    background-color: #001f3d;
     z-index: 100;
-    height: 100vh;
-  }
 
-  h1 {
-    position: absolute;
-    margin: 0;
-
-    object {
-      transform-origin: bottom left;
+    .name {
+      z-index: 1;
+      left: 2%;
+      width: 90vw;
+      filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.6));
+      margin-top: auto;
+      position: relative;
+      transform: translateY(15%);
     }
   }
-
-  .close {
-    position: absolute;
-    left: 2.5vw;
-    bottom: 2.5vw;
-    cursor: pointer;
-    user-select: none;
+  .line {
+    position: fixed;
+    top: 50vh;
+    width: 100vw;
+    height: 1.4vw;
+    background-color: #820a0a;
+    z-index: 100;
   }
 
-  .arrow-enter-active {
-    transition: opacity 0.5s cubic-bezier(0.55, 0.085, 0.68, 0.53) 2s;
-  }
-  .arrow-leave-active {
-    transition: opacity 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94);
-  }
-  .arrow-enter {
-    opacity: 0;
-  }
-  .arrow-leave-to {
-    opacity: 0;
-  }
+  .bottom {
+    height: 50vh;
+    padding-top: calc(50vh + 1.4vw);
+    min-height: fit-content;
+    padding-bottom: 55px;
+    background-color: #000a14;
 
-  .fade-in-enter {
-    opacity: 0;
+    .titles {
+      position: relative;
+      top: 5px;
+      font-size: calc(50vw / 9);
+      line-height: 1;
+      right: 2%;
+      font-family: Raleway, Helvetica, sans-serif;
+      float: right;
+
+      .web {
+        font-weight: 200;
+        color: white;
+      }
+      .game {
+        font-weight: 400;
+        color: rgba(232, 232, 247, 0.8);
+      }
+      .and {
+        position: absolute;
+        transform: translateX(calc(-100% - 10px));
+        font-weight: 200;
+        color: rgba(225, 225, 255, 0.3);
+      }
+    }
   }
-  .fade-in-enter-active {
-    transition: 500ms opacity ease-out;
-  }
+}
 </style>
 
 
 <script lang="ts">
-  import { Component, Prop, Vue, Watch } from "vue-property-decorator";
-  import Titles from "@/components/titles.vue";
-  import Continue from "@/components/continue.vue";
-  import NavLinks from "@/components/nav_links.vue";
-  import { tween } from "shifty";
-
-  @Component({
-    components: { Titles, Continue, NavLinks }
-  })
-  export default class NavBar extends Vue {
-    intro_transitioning: boolean = false;
-    titles_visible: boolean = false;
-    close_visible: boolean = false;
-    show_links: boolean = false;
-    underline_length: number = 0;
-    delay_underline: boolean = false;
-    window_height = window.innerHeight;
-    window_width = window.innerWidth;
-
-    @Prop() uncollapsed! : boolean;
-    @Prop() position! : number;
-
-    created(): void {
-      this.titles_visible = this.uncollapsed;
-      this.delay_underline = this.uncollapsed;
-
-      let vm = this;
-      window.addEventListener("resize", function() {
-        vm.window_height = window.innerHeight;
-        vm.window_width = window.innerWidth;
-      });
-    }
-
-    show_close(): void {
-      this.close_visible = true;
-
-      let vm = this;
-      let close_hander = function() {
-        vm.close_titles();
-
-        window.removeEventListener("click", close_hander);
-        window.removeEventListener("keydown", close_hander);
-        window.removeEventListener("wheel", close_hander);
-      };
-
-      window.addEventListener("click", close_hander);
-      window.addEventListener("keydown", close_hander);
-      window.addEventListener("wheel", close_hander);
-    }
-    close_titles(): void {
-      this.titles_visible = false;
-      this.close_visible = false;
-    }
-    close_intro(): void {
-      this.$emit("close_intro");
-    }
-
-    underline_enter(): void {
-      tween({
-        from: { x: 0 },
-        to: { x: 1600 * this.scale_factor },
-        duration: 750,
-        easing: "easeInOutQuad",
-        delay: this.delay_underline ? 3250 : 750,
-        step: state => {
-          this.underline_length = state.x;
-        }
-      }).then( () => {
-        this.show_links = true;
-      });
-    }
-
-    @Watch("titles_visible")
-    onTitlesChanged(): void {
-      this.intro_transitioning = true;
-
-      setTimeout(() => {
-        this.intro_transitioning = false;
-      }, 3000);
-    }
-
-    get scale_factor(): number {
-      if  (window.matchMedia("(max-width: 1024px) and (orientation: portrait)").matches) {
-          return 0.22
-      }
-      else {
-        return 0.3
-      }
-    }
-  }
+import Vue from "vue";
+export default Vue.extend({});
 </script>
