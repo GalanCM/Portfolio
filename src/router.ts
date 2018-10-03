0e531261ac55b512d0d3f71db7af93a7641f2f48
Galan Montgomery <galan.c.m@gmail.com>
2018-10-03 14:07:43 -0400

    A scrollBehaviour to router.

--- src/router.ts
+++ src/router.ts
@@ -36,5 +36,12 @@ export default new Router({
       component: About,
       meta: { title: "About" }
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
