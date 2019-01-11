23962280d2ec02a9036c3f3337758f37d69e6a5f
Galan Montgomery <galan.c.m@gmail.com>
2019-01-10 20:17:43 -0500

    Restore dynamic metadata.

---
+++ src/metadata.ts
@@ -0,0 +1,57 @@
+export default [
+  {
+    name: "description",
+    content:
+      "Professional homepage for web developer and designer Galan Montgomery. About, Code Samples, and Portfolio."
+  },
+  {
+    property: "og:title",
+    content: "Galan Montgomery: Web Developer and Designer"
+  },
+  { property: "og:site_name", content: "galan-montgomery.com" },
+  { property: "og:type", content: "website" },
+  {
+    property: "og:url",
+    content: "https://www.galan-montgomery.com"
+  },
+  {
+    property: "og:image",
+    content: "https://www.galan-montgomery.com/opengraph_screenshot.png"
+  },
+  {
+    property: "og:description",
+    content:
+      "Professional homepage for Galan Montgomery. Learn more about me, and view my work."
+  },
+  { name: "twitter:card", content: "summary" },
+  {
+    name: "twitter:site",
+    content: "@_galan_m_"
+  },
+  {
+    name: "twitter:title",
+    content: "Galan Montgomery: Web Developer and Designer"
+  },
+  {
+    name: "twitter:description",
+    content:
+      "Professional homepage for Galan Montgomery. Learn more about me, and view my work."
+  },
+  {
+    name: "twitter:image:src",
+    content: "https://www.galan-montgomery.com/opengraph_screenshot.png"
+  },
+  {
+    itemprop: "name",
+    content: "Galan Montgomery: Web Developer and Designer"
+  },
+  {
+    itemprop: "description",
+    content:
+      "Professional homepage for Galan Montgomery. Learn more about me, and view my work."
+  },
+  {
+    itemprop: "image",
+    content: "https://www.galan-montgomery.com/opengraph_screenshot.png"
+  }
+];
